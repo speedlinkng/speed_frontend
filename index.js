64421407b92fcadc9fef7e8afcf48887c92be994
drrowly99 <drrowly99@gmail.com>
2023-10-29 08:00:24 +0100

    creating the new branch adjusting routes

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const corsOptions ={
 }
 app.set('view engine', 'ejs');
 
-
 // app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'static/dashboard')));
