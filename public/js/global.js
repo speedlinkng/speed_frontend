ea6324b30de2022f051ec309df1d96109d17f5c3
drrowly99 <drrowly99@gmail.com>
2023-10-29 07:55:52 +0100

    creating the new branch adjusting routes

---
+++ public/js/global.js
@@ -0,0 +1,1 @@
+export const site_url = 'https://speedlink-frontend.onrender.com';
---
+++ public/js/loading.js
@@ -0,0 +1,15 @@
+// Another JavaScript file
+import { myGlobalVariable } from './globals.js';
+
+function start_loading(myButton){
+    // Change the text to "Loading..." with animated dots
+    myButton.innerHTML = 'Loading<span class="loading-dots"></span>';
+    myButton.classList.add('disabled:pointer-events-none', 'disabled:select-none', 'disabled:opacity-60');
+    myButton.disabled = true;
+}
+
+function end_loading(myButton){
+    myButton.classList.remove('disabled:pointer-events-none', 'disabled:select-none', 'disabled:opacity-60');
+    myButton.disabled = false; // Re-enable the button (whether the fetch succeeds or fails)
+    myButton.innerHTML = 'Sign In';
+}
