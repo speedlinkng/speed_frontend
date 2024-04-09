37e7354032aebc95f053d3ed89d614b31be9b6a7
drrowly99 <drrowly99@gmail.com>
2024-04-09 04:46:37 -0700

    fixes

--- views/dashboard/form/script.js
+++ views/dashboard/form/script.js
@@ -152,7 +152,7 @@ const conditionSymbolMap = {
      
 
         async downloadDoc(){
-          alert('clicked downalodDoc')
+          alert(backendUrl)
           var _allReplyLink = this.allReplyLink
           var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
                 var postHtml = "</body></html>";
