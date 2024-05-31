1eacb52069ce673c4a439d357ba7454e7fba9b8c
drrowly99 <drrowly99@gmail.com>
2024-05-31 16:53:57 -0700

    fixed non scrolling table again on dashboard

--- public/js/module/global.js
+++ public/js/module/global.js
@@ -2,7 +2,7 @@
     const baseUrl = `${window.location.protocol}//${window.location.host}`;
     const backendUrl = `https://speedbankend-production.up.railway.app`;
     const backendUrl_ = `https://speedbankend-production.up.railway.app`; 
-    window.bkupfile = 0
+    window.bkupfile = 0 
     const allArray = { 
       values: {},
       preferred: localStorage.getItem('preferred'),
