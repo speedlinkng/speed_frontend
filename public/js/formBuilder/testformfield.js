78e8addbddac995f15c5e0cf11af314d18b4c391
drrowly99 <drrowly99@gmail.com>
2024-06-15 11:20:23 -0700

    fixs

--- public/js/formBuilder/testformfield.js
+++ public/js/formBuilder/testformfield.js
@@ -100,10 +100,10 @@ newInput = $(
   <div class="grid grid-cols-12 gap-3 isOpenField${count}" x-data="{ isOpenField${count}: true }" :class="{ 'opacity-0': !isOpenField${count}, 'pointer-events-none': !isOpenField${count} }">
     <div class="col-span-12 eachField${count} text-sm eachField grid grid-cols-1 w-full mt-2 case-prime${count} rounded-lg border border-slate-200 p-3 py-4 dark:border-navy-600">
      <div class="cursor-move w-fit z-50" ondragstart="getPrevFields(${count}, ${addmore_count}, ${true})">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
       </svg>
-    move
+    
       </div>
       <div class="flex space-x-4">
 
