6a253701706aa641f9de19b28f9c9bd797f05bd4
drrowly99 <drrowly99@gmail.com>
2024-06-15 11:18:19 -0700

    fixs

--- views/dashboard/form/script.js
+++ views/dashboard/form/script.js
@@ -542,11 +542,16 @@ document.addEventListener("alpine:init", () => {
           var result = Group_by.map((fieldName) => { 
 
        
-              const date = new Date();
-              const year = date.getFullYear();
-              const month = ('0' + (date.getMonth() + 1)).slice(-2);
-              const day = ('0' + date.getDate()).slice(-2);
-              let getCurrentFormattedDate = `${year}-${month}-${day}`;
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = ('0' + (date.getMonth() + 1)).slice(-2);
+            const day = ('0' + date.getDate()).slice(-2);
+            const hours = ('0' + date.getHours()).slice(-2);
+            const minutes = ('0' + date.getMinutes()).slice(-2);
+            const seconds = ('0' + date.getSeconds()).slice(-2);
+            
+            let getCurrentFormattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+            
             
             
             fieldName = getCurrentFormattedDate + "_DefaultSubmission";
