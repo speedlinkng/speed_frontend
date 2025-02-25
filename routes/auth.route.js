dfcd8ac6a932ea6071b853d13c3f05dfbe79310b
drrowly99 <drrowly99@gmail.com>
2025-02-25 19:43:38 +0100

    logout

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -9,10 +9,19 @@ const {saveUserSession} = require('../handlers/Session_handler');
 
 
 
-  router.get('/signin', saveUserSession);
+  // router.get('/signin', saveUserSession);
 
   router.get('/signup', function(req, res) {
     res.render(`auth/auth.ejs`, {urls: {base: process.env.BASU_URL, backend: process.env.BACKEND_URL},activeFile:"signup", data: null, title: "Authorization"});
+  });
+router.get('/signin', function (req, res) {
+  const { error } = req.query
+  console.log(error)
+
+  if (error) {
+    res.render(`auth/auth.ejs`, {urls: {base: process.env.BASU_URL, backend: process.env.BACKEND_URL}, activeFile:"signin", data: null,  error: error, baseUrl: process.env.BASE_URL});
+  }
+    res.render(`auth/auth.ejs`, {urls: {base: process.env.BASU_URL, backend: process.env.BACKEND_URL},activeFile:"signin", data: null, title: "Authorization"});
   });
 
   router.get("/verify", async function (req, res) {
@@ -54,17 +63,37 @@ const {saveUserSession} = require('../handlers/Session_handler');
 });
 
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
+  const { error } = req.query
+  console.log(error)
+  if (error) {
+    res.render(`auth/activate.ejs`, {urls: {backend: process.env.BACKEND_URL}, activeFile:null, data: null,  error: error, baseUrl: process.env.BASE_URL});
+  }
   res.render(`auth/auth.ejs`, {urls: {base: process.env.BASU_URL, backend: process.env.BACKEND_URL},activeFile:null, data: null, title: "Authorization"});
 });
 
-router.get('/:data', function(req, res) {
-  res.render(`auth/auth.ejs`, {urls: {base: process.env.BASU_URL, backend: process.env.BACKEND_URL},activeFile:null, data: null, title: "Authorization"});
+router.get('/activate/:data', function (req, res) {
+  let data = req.params.data
+  const { error } = req.query
+  console.log(error)
+  console.log(data)
+  if (error) {
+    res.render(`auth/activate.ejs`, {urls: {backend: process.env.BACKEND_URL}, activeFile:data, data: data,  error: error, baseUrl: process.env.BASE_URL});
+  }
 });
 
+router.get('/forget', function (req, res) {
+
+  const { error } = req.query
+  console.log(error)
+  if (error) {
+    res.render(`auth/activate.ejs`, {urls: {backend: process.env.BACKEND_URL}, activeFile:data, data: data,  error: error, baseUrl: process.env.BASE_URL});
+  }
+});
   
 router.get('/activate', function (req, res) {
-  const { error } = req.body
+  const { error } = req.query
+  console.log(error)
   if (error) {
     res.render(`auth/activate.ejs`, {urls: {backend: process.env.BACKEND_URL}, activeFile:null, data: null,  error: error, baseUrl: process.env.BASE_URL});
   }
@@ -72,47 +101,47 @@ router.get('/activate', function (req, res) {
   });
 
 
-  router.get('/activate/:activateId', function (req, res) {
-      // activate code is a jwt token.
-      // that expires in 30 minutes.
-      // check if the actiivate code has a user then activate that user.
+  // router.get('/activate/:activateId', function (req, res) {
+  //     // activate code is a jwt token.
+  //     // that expires in 30 minutes.
+  //     // check if the actiivate code has a user then activate that user.
     
 
-      let activateToken = req.params.activateId
-      jwt.verify(activateToken, process.env.REFRESH_TOK_SEC, (err, decoded) => {
-        if (err) {
-          // console.log(err)
-          res.render(`auth/activate.ejs`, {urls: {backend: process.env.BACKEND_URL}, activeFile:null, data: null, error: err, baseUrl: process.env.BASE_URL});
-        } 
-        let decodedToken = decoded
-        const decodedUser = decodedToken.result.user_id;
-        // check if user id exists in the database, if it does, set activated
-        console.log(decodedUser)
-        request(
-          {
-            method: "GET",
-            url: process.env.BACKEND_URL + `/api/users/activateuser/${decodedUser}`
-          },
-          (err, response, body) => {
-            if (err) {
-              console.log(err);
+  //     let activateToken = req.params.activateId
+  //     jwt.verify(activateToken, process.env.REFRESH_TOK_SEC, (err, decoded) => {
+  //       if (err) {
+  //         // console.log(err)
+  //         res.render(`auth/activate.ejs`, {urls: {backend: process.env.BACKEND_URL}, activeFile:null, data: null, error: err, baseUrl: process.env.BASE_URL});
+  //       } 
+  //       let decodedToken = decoded
+  //       const decodedUser = decodedToken.result.user_id;
+  //       // check if user id exists in the database, if it does, set activated
+  //       console.log(decodedUser)
+  //       request(
+  //         {
+  //           method: "GET",
+  //           url: process.env.BACKEND_URL + `/api/users/activateuser/${decodedUser}`
+  //         },
+  //         (err, response, body) => {
+  //           if (err) {
+  //             console.log(err);
               
-            } else {
-              let status = response.statusCode
-              if (status == 301) {
-                // then no match was found
-              }
-              if (status == 200) {
-                res.render(`auth/auth.ejs`, { urls: {backend: process.env.BACKEND_URL}, activeFile:null, data: null, error: null, baseUrl: process.env.BASE_URL });
-              }
+  //           } else {
+  //             let status = response.statusCode
+  //             if (status == 301) {
+  //               // then no match was found
+  //             }
+  //             if (status == 200) {
+  //               res.render(`auth/auth.ejs`, { urls: {backend: process.env.BACKEND_URL}, activeFile:null, data: null, error: null, baseUrl: process.env.BASE_URL });
+  //             }
       
-            }
-          }
-        );
+  //           }
+  //         }
+  //       );
 
-      })
+  //     })
 
-  });
+  // });
 
 
 module.exports = router
