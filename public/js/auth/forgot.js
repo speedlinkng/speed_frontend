28a3bfd2b82755e33e47ae24c1a21207b5676f87
drrowly99 <drrowly99@gmail.com>
2025-02-26 10:32:04 +0100

    forgot double fix

--- public/js/auth/forgot.js
+++ public/js/auth/forgot.js
@@ -81,6 +81,50 @@ $(document).ready(function () {
       }, 1000);
     }
   }
+  async function forgot_btn_(e) {
+    let settings = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify({
+        email: $('#forgot_email').val()
+      })
+    };
+
+    const myButton = document.getElementById('forgot_btn');
+    // Call loading function
+    start_loading(myButton);
+
+    try {
+      let fetchResponses = await fetch(`${backendUrl}/api/users/forgot`, settings);
+      let sta = await fetchResponses.status;
+      let json = await fetchResponses.json();
+
+      if (sta == 200) {
+        $('.forgot_success').show();
+        $('.forgot_success').text('A password reset link has been sent to your email address');
+        setTimeout(function () {
+          $('.forgot_success').text('');
+          $('.forgot_success').hide();
+        }, 5000);
+
+        $('.forgot').hide();
+        $('.preverify').show();
+        countdown(); // Start the countdown
+      } else if (sta == 302 || sta == 303 || sta == 403) {
+        // Handle other status codes if needed
+      } else {
+        // Handle other cases
+      }
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setTimeout(function () {
+        // end_loading(myButton, 'Send Link');
+      }, 1000);
+    }
+  }
 
   $("#forgot_btn").click(async function (e) {
     forgot_btn();
@@ -89,7 +133,7 @@ $(document).ready(function () {
   $("#resend_btn").click(async function (e) {
     if ($('#countdown').text() == '0') {
       alert('countdownlicked')
-      forgot_btn();
+      forgot_btn_();
     }
   });
 });
