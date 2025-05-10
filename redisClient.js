981d3b241919249d677b4506524edf8781b9c172
drrowly99 <drrowly99@gmail.com>
2025-05-10 19:00:02 +0100

    fixing dashboard

--- redisClient.js
+++ redisClient.js
@@ -3,6 +3,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const redisClient = new Redis('redis://default:gWSYLKZdGRaLhedkjlOUmabtWIMDntxv@hopper.proxy.rlwy.net:58345');
+const redisClient = new Redis('redis://default:gWSYLKZdGRaLhedkjlOUmabtWIMDntxv@redis.railway.internal:6379');
 
 module.exports = redisClient;
